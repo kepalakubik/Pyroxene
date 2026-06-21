3dc2c8569dbe2a3b3f8afec9c27f90e5d9221f77
kepalakubik <ryan@kepalakubik.my.id>
2026-06-21 14:39:08 +0800

    perf: optimize the onClientTick

--- src/fabric/java/id/kepalakubik/minecraftbluearchivehalo/BlueArchiveHalosPyroxeneClient.java
+++ src/fabric/java/id/kepalakubik/minecraftbluearchivehalo/BlueArchiveHalosPyroxeneClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 
@@ -21,12 +22,13 @@ public class BlueArchiveHalosPyroxeneClient implements ClientModInitializer {
     }
 
     private void onClientTick(Minecraft minecraft) {
+        if (HaloHeadSpringTracker.isEmpty()) return;
         if (minecraft.level == null || minecraft.isPaused()) return;
 
-        for (Player player : minecraft.level.players()) {
-            if (!(player.getItemBySlot(EquipmentSlot.HEAD).getItem() instanceof HaloItem)) {
-                HaloHeadSpringTracker.remove(player.getId());
-            }
-        }
+        HaloHeadSpringTracker.removeIf(id -> {
+            Entity entity = minecraft.level.getEntity(id);
+            return !(entity instanceof Player player)
+                || !(player.getItemBySlot(EquipmentSlot.HEAD).getItem() instanceof HaloItem);
+        });
     }
 }
