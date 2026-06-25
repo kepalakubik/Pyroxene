ccb02bf2f9d27ce95611343d4585c49cd8184db8
kepalakubik <ryan@kepalakubik.my.id>
2026-06-25 23:03:47 +0800

    chore (BlueArchiveHalosPyroxeneClient): supress unused warning

--- fabric/src/main/java/id/kepalakubik/minecraftbluearchivehalo/BlueArchiveHalosPyroxeneClient.java
+++ fabric/src/main/java/id/kepalakubik/minecraftbluearchivehalo/BlueArchiveHalosPyroxeneClient.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 
+@SuppressWarnings("unused")
 public class BlueArchiveHalosPyroxeneClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
